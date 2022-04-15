bec84b68d9270a801ff16486a0c3a2d81f293c6f
nrm <sanew102@gmail.com>
2022-04-15 07:01:47 +0600

    Initial Commit

---
+++ Lab_2_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab_2_1
+//
+//  Created by Нурым Нагиметов on 15.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
